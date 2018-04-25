4e129041e919560691446a3493c909379d0d06f4
stevenchendan <qwe16165850@gmail.com>
2018-04-25 23:33:25 +1000

    ShortestWord

---
+++ codewars/JavaScript/ShortestWord.js
@@ -0,0 +1,19 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+//my solution
+function findShort(s){
+    let array = s.split(' ');
+    array.sort(function(a, b) {
+      return a.length - b.length;
+    })
+    return array[0].length;
+}
+
+//best solution
+function findShort(s){
+    return Math.min.apply(null, s.split(' ').map(w => w.length));
+}
